b091258327fa9b4c8320afe8ebe201196c1e64b1
daka1 <davidcaetanoribeiro@gmail.com>
2025-06-12 09:30:48 -0300

    Update ParametrosSistema.java
    Correçao

--- src/service/ParametrosSistema.java
+++ src/service/ParametrosSistema.java
@@ -2,7 +2,7 @@ package service;
 
 public class ParametrosSistema {
 
-    private static double custoPorKw = 2650.00;
+    private static double custoPorKw = 3000.0;
     private static double fatorEconomia = 0.75;
     private static double percentualGeracao = 0.95; // 95%
 
